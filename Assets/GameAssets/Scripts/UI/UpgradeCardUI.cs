6092e6e90ae382a0309db47dab060aa27be4686f
LiquidIce64 <gilyazov.rm2005@gmail.com>
2026-06-07 14:23:57 +0300

    Show upgrade button on level up + small fixes

--- Assets/GameAssets/Scripts/UI/UpgradeCardUI.cs
+++ Assets/GameAssets/Scripts/UI/UpgradeCardUI.cs
@@ -5,14 +5,6 @@ public class UpgradeCardUI : MonoBehaviour
 {
     [SerializeField] private Image cardImage;
 
-    [SerializeField] private Sprite damageCard;
-    [SerializeField] private Sprite attackSpeedCard;
-    [SerializeField] private Sprite xpMagnetCard;
-    [SerializeField] private Sprite explosionRadiusCard;
-    [SerializeField] private Sprite healthCard;
-    [SerializeField] private Sprite walkSpeedCard;
-    [SerializeField] private Sprite shockwaveCard;
-
     private BaseUpgrade upgrade;
     private UpgradeMenuManager menuManager;
 
@@ -21,20 +13,7 @@ public class UpgradeCardUI : MonoBehaviour
         upgrade = newUpgrade;
         menuManager = manager;
 
-        if (upgrade is DamageUpgrade)
-            cardImage.sprite = damageCard;
-        else if (upgrade is FireRateUpgrade)
-            cardImage.sprite = attackSpeedCard;
-        else if (upgrade is XPMagnetUpgrade)
-            cardImage.sprite = xpMagnetCard;
-        else if (upgrade is ExplosionRadiusUpgrade)
-            cardImage.sprite = explosionRadiusCard;
-        else if (upgrade is HealthUpgrade)
-            cardImage.sprite = healthCard;
-        else if (upgrade is SpeedUpgrade)
-            cardImage.sprite = walkSpeedCard;
-        else if (upgrade is ShockwaveUpgrade)
-            cardImage.sprite = shockwaveCard;
+        cardImage.sprite = upgrade.cardSprite;
     }
 
     public void OnTakeClicked()
